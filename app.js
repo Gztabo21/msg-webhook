0114e8a3966b658c279a0035369e01bd75124982
Gztabo21 <Gustavocacharuco@gmail.com>
2020-07-07 23:48:16 -0400

    mejoras

--- app.js
+++ app.js
@@ -7,12 +7,50 @@ function handleMessage(sender_psid, received_message) {
     let res;
     if(received_message.text){
         res = {"text": `You sent the message: "${received_message.text}". Now send me an image!`}
+    }else if (received_message.attachments) {
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+      }
     }
     callSendAPI(sender_psid, res);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
+    let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 
 }
 
